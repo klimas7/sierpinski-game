001a6f882b931cb137246590f767118f9f29fdec
klimas7 <klimas7@gmail.com>
2020-12-30 21:14:16 +0100

    Initial commit

---
+++ src/main/java/pl/klimas7/sieripinski/game/SieripinskiGameApplication.java
@@ -0,0 +1,13 @@
+package pl.klimas7.sieripinski.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SieripinskiGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SieripinskiGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/klimas7/sieripinski/game/SieripinskiGameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klimas7.sieripinski.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SieripinskiGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
